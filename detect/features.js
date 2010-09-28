83df8ad8399e8b6c9c2a54be9deaf8fe0fe726b2
Bryan Forbes <bryan@reigndropsfall.net>
2010-09-28 15:37:15 -0500

    Revamped Array tests and XHR test.  Added Object tests.

--- detect/features.js
+++ detect/features.js
@@ -3,31 +3,64 @@
     // FIXME: break this out into "modules", like array.js, dom.js, lang.js (?) ^ph
 
     var addtest = has.add;
-   
+
+    // Array tests
     addtest("native-forEach", function(){
-        return !!("forEach" in []);
+        return "forEach" in [];
     });
     addtest("native-isArray", function(){
-        return !!("isArray" in Array);
+        return "isArray" in Array;
     });
     addtest("native-map", function(){
-        return !!([].map);
+        return "map" in [];
     });
     addtest("es5-array", function(){
         var ar = [];
-        return !!(has("native-isArray") && ar.indexOf && ar.lastIndexOf && ar.every && ar.some &&
-            has("native-forEach") && has("native-map") && ar.filter && ar.reduce && ar.reduceRight);
+        return has("native-isArray") && ("indexOf" in ar) && ("lastIndexOf" in ar) &&
+            ("every" in ar) && ("some" in ar) && has("native-forEach") &&
+            has("native-map") && ("filter" in ar) && ("reduce" in ar) && ("reduceRight" in ar);
     });
-    
+
+    // Function tests
+    addtest("native-bind", function(){
+        return "bind" in Function.prototype;
+    });
     addtest('function-caller', (function(undefined) { 
       function test() { return test.caller !== undefined }
       return test();
     })());
-    
+
+    // Object tests
+    addtest("object-create", function(){
+        return "create" in Object;
+    });
+    addtest("object-getPrototypeOf", function(){
+        return "getPrototypeOf" in Object;
+    });
+    addtest("object-seal", function(g){
+        var o = Object;
+        return ("seal" in o) && ("freeze" in o) && ("isSealed" in o) && ("isFrozen" in o);
+    });
+    addtest("object-keys", function(){
+        return "keys" in Object;
+    });
+    addtest("object-extensible", function(){
+        return ("preventExtensions" in Object) && ("isExtensible" in Object);
+    });
+    addtest("object-properties", function(){
+        var o = Object;
+        return ("defineProperty" in o) && ("defineProperties" in o) &&
+            ("getOwnPropertyDescriptor" in o) && ("getOwnPropertyNames" in o);
+    });
+    addtest("es5-object", function(){
+        return has("object-create") && has("object-getPrototypeOf") && has("object-seal") &&
+            has("object-keys") && has("object-extensible") && has("object-properties");
+    });
+
+    // JSON tests
     addtest("json-parse", function(global){
         return !!("JSON" in global && typeof JSON.parse == "function" && JSON.parse('{"a":true}').a);
     });
-
     addtest("json-stringify", function(global){
         return !!("JSON" in global && typeof JSON.stringify == "function" && JSON.stringify({a:true}) == '{"a":true}');
     });
@@ -35,11 +68,6 @@
     // FIXME: isn't really native
     addtest("native-console", function(global){
         return !!("console" in global)
-    });
-
-    // FIXME: poorly named, might be useless ^ph
-    addtest("beget", function(){
-        !!("create" in Object)
     });
 
     if(!has('is-browser')){ return; }
@@ -56,24 +84,24 @@
         var http, ret;
         try{
             http = new XMLHttpRequest();
-            if(http){ 
+            if(http){
                 ret = new Boolean(true); 
                 ret.ACTIVEX = false; 
             }
-        }catch(e){ 
-            for(var i = 0, l = xhrTests.length; i < l; i++){
-                var xhr = xhrTests[i];
+        }catch(e){
+            for(var i = 0, xhr; xhr = xhrTests[i]; i++){
                 try{
                     http = new ActiveXObject(xhr);
                 }catch(e){}
-                if(http){ 
+                if(http){
                     // FIXME: should this be true and sniff ACTIVEX
                     ret = new Boolean(false);
                     ret.ACTIVEX = xhr;
-                    delete xhrTests;
+                    break;
                 }
             }
         }
+        delete xhrTests;
         return ret;
     });
 
