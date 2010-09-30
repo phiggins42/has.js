8e6f813ed0558ba0d7f4e78b7617be212c5e49b0
jdalton <john.david.dalton@gmail.com>
2010-09-30 08:37:03 -0400

    has.js: Fix array.js tests and add more ES5 array method tests. [jddalton]

--- detect/array.js
+++ detect/array.js
@@ -1,32 +1,61 @@
 (function(has, addtest, cssprop){
-
+    
     // Array tests
+    addtest("array-every", function(){
+        return typeof [].every == "function";
+    });
+    
+    addtest("array-filter", function(){
+        return typeof [].filter == "function";
+    });
+    
     addtest("array-foreach", function(){
-        return "forEach" in [];
+        return typeof [].forEach == "function";
     });
-
+    
+    addtest("array-indexof", function(){
+        return typeof [].indexOf == "function";
+    });
+    
     addtest("array-isarray", function(){
-        return "isArray" in Array && Array.isArray([]);
+        return typeof Array.isArray == "function" && Array.isArray([]);
     });
-
+    
+    addtest("array-lastindexof", function(){
+        return typeof [].lastIndexOf == "function";
+    });
+    
     addtest("array-map", function(){
-        return "map" in [];
+        return typeof [].map == "function";
+    });
+    
+    addtest("array-reduce", function(){
+        return typeof [].reduce == "function";
+    });
+    
+    addtest("array-reduceright", function(){
+        return typeof [].reduce == "function";
+    });
+    
+    addtest("array-some", function(){
+        return typeof [].map == "function";
     });
     
     addtest("array-es5", function(){
-        var ar = [];
-        // FIXME: should the 'some' and 'reduce' et al be addtests() too? ^ph
-        return has("array-isarray") && ("indexOf" in ar) && ("lastIndexOf" in ar) &&
-            ("every" in ar) && ("some" in ar) && has("array-foreach") &&
-            has("array-map") && ("filter" in ar) && ("reduce" in ar) && ("reduceRight" in ar);
+        return has("array-every") && has("array-filter") && has("array-foreach") &&
+            has("array-indexof") && has("array-isarray") && has("array-lastindexof") &&
+            has("array-map") && has("array-reduce") && has("array-reduceright") &&
+            has("array-some");
     });
     
-    addtest('array-slice-nodelist', function(g, d){
-        try{
-            return (Array.prototype.slice.call(d.forms, 0) instanceof Array);
-        }catch(e){
-            return false;
-        }
+    addtest('array-slice-nodelist', function(g, d, el){
+        var supported = true, de = d.documentElement, id = de.id;
+        // Opera 9.25 bug
+        de.id = 'length';
+        // older Safari will return an empty array
+        try { supported = !![].slice.call(d.childNodes, 0)[0]; } catch(e) { }
+        de.id = id;
+        return supported;
     });
 
 })(has, has.add, has.cssprop);
