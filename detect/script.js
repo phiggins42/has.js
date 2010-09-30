c33df9e4a211526826c0054e7d3b3f5691951c02
rwldrn <waldron.rick@gmail.com>
2010-09-29 20:24:55 -0400

    Tests for defer, async on script tags; New file: script.js

---
+++ detect/script.js
@@ -0,0 +1,16 @@
+(function(has, addtest, cssprop){
+
+
+
+    var script = document.createElement('script');
+    
+
+    addtest("script-defer", function() {
+        return !!("defer" in script);
+    });
+
+    addtest("script-async", function(){
+        return !!("async" in script);
+    });    
+
+})(has, has.add, has.cssprop);
