8892290e1f280c3a0d98377ccea3f4890e3457e7
Peter E Higgins <dante@dojotoolkit.org>
2010-10-01 08:29:52 +0200

    shortened video tests

--- detect/video.js
+++ detect/video.js
@@ -7,18 +7,15 @@
     });
 
     addtest("video-h264-baseline", function() {
-        if (!has("video")) { return false; }
-        return video.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
+        return has("video") && video.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
     });
 
     addtest("video-ogg-theora", function() {
-        if (!has("video")) { return false; }
-        return video.canPlayType('video/ogg; codecs="theora, vorbis"');
+        return has("video") && video.canPlayType('video/ogg; codecs="theora, vorbis"');
     });
 
     addtest("video-webm", function() {
-        if (!has("video")) { return false; }
-        return video.canPlayType('video/webm; codecs="vp8, vorbis"');
+        return has("video") && video.canPlayType('video/webm; codecs="vp8, vorbis"');
     });
 
 })(has, has.add, has.cssprop);
