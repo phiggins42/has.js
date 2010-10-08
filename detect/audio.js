afd4f9566229711bbd02ecbdb6a9d0f4e667240e
Peter E Higgins <dante@dojotoolkit.org>
2010-10-08 14:41:44 -0400

    stuff from modernizr, notes.

---
+++ detect/audio.js
@@ -0,0 +1,29 @@
+(function(has, addtest, cssprop){
+
+    var STR = "string",
+        FN = "function"
+    ;
+
+    var audio = document.createElement("audio");
+
+    addtest("audio", function(){
+        return has.isHostType(audio, "canPlayType");
+    });
+    
+    addtest("audio-ogg", function(){
+        return has("audio") && audio.canPlayType("audio/ogg; codecs='vorbis'");
+    });
+    
+    addtest("audio-mp3", function(){
+        return has("audio") && audio.canPlayType("audio/mpeg;");
+    });
+    
+    addtest("audio-wav", function(){
+        return has("audio") && audio.canPlayType("audio/wav; codecs='1'");
+    });
+    
+    addtest("audio-m4a", function(){
+        return has("audio") && (audio.canPlayType("audio/x-m4a;") || audio.canPlayType("audio/aac;"));
+    });
+
+})(has, has.add, has.cssprop);
