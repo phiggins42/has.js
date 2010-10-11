b7af7351aad3c03a583ac41772e7f96d4aa7aa2b
Bryan Forbes <bryan@reigndropsfall.net>
2010-10-10 22:18:48 -0500

    Added tests for buggy String.trim with whitespace and BOM and RegExp with BOM.

--- detect/bugs.js
+++ detect/bugs.js
@@ -366,10 +366,34 @@
         return true;
     });
 
+    var whitespace = "\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002"+
+        "\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029";
+    var bom = "\uFEFF";
+
     addtest("bug-regexp-whitespace", function(){
-        var str = "\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002"+
-                "\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029";
-        return !/^\s+$/.test(str);
+        return !/^\s+$/.test(whitespace);
+    });
+
+    // ES5 added <BOM> as a whitespace character
+    addtest("bug-regexp-bom", function(){
+        return !/^\s+$/.test(bom);
+    });
+
+    addtest("bug-trim-whitespace", function(){
+        var buggy = null;
+        if(has("string-trim")){
+            buggy = !!whitespace.trim();
+        }
+        return buggy;
+    });
+
+    // ES5 added <BOM> as a whitespace character
+    addtest("bug-trim-bom", function(){
+        var buggy = null;
+        if(has("string-trim")){
+            buggy = !!bom.trim();
+        }
+        return buggy;
     });
 
     addtest("bug-tofixed-rounding", function(){
