7d997db480ea5ee8a7200fd04e75bab8bb6718e9
jdalton <john.david.dalton@gmail.com>
2010-10-16 01:52:34 -0400

    has.js: Add native-navigator test, remove out of project scope native-has-attr test, and cleanup other tests in features.js. [jddalton]

--- detect/features.js
+++ detect/features.js
@@ -1,8 +1,8 @@
-(function(has, addtest, cssprop){
+(function(has, addtest, cssprop, undefined){
 
     var STR = "string",
         FN = "function"
-    ;   
+    ;
 
     // FIXME: isn't really native
     // miller device gives "[object Console]" in Opera & Webkit. Object in FF, though. ^pi
@@ -10,20 +10,18 @@
         return ("console" in g);
     });
 
-    if(!has("dom")){ return; }
-
     addtest("native-xhr", function(g){
         return has.isHostType(g, "XMLHttpRequest");
     });
-    
+
     addtest("native-cors-xhr", function(g){
-        return has('native-xhr') && "withCredentials" in new XMLHttpRequest;
+        return has("native-xhr") && ("withCredentials" in new XMLHttpRequest);
     });
-    
+
     addtest("native-xhr-uploadevents", function(g){
-        return has('native-xhr') && "upload" in new XMLHttpRequest;
+        return has("native-xhr") && ("upload" in new XMLHttpRequest);
     });
-    
+
     addtest("activex", function(g){
         return has.isHostType(g, "ActiveXObject");
     });
@@ -32,7 +30,7 @@
         var supported = null;
         if(has("activex")){
             try{
-                supported = !!(new ActiveXObject("htmlfile"));
+                supported = !!new ActiveXObject("htmlfile");
             }catch(e){
                 supported = false;
             }
@@ -40,36 +38,10 @@
         return supported;
     });
 
-    // FROM cft.js
-    addtest("native-has-attr", function(g, d){
-        if(d.createElement){
-            var i = d.createElement("iframe"),
-                root = d.documentElement,
-                frames = g.frames;
-            if(root && root.appendChild && root.removeChild){
-                i.style.display = "none";
-                root.appendChild(i);
-                // some clients (e.g. Blackberry 9000 (Bold)) throw error when accesing frame's document
-                try{
-                    var frame = frames[frames.length-1];
-                    if(frame){
-                        var doc = frame.document;
-                        if(doc && doc.write){
-                            doc.write("<html><head><title></title></head><body></body></html>");
-                            var present = doc.documentElement ? ("hasAttribute" in doc.documentElement) : false;
-                            root.removeChild(i);
-                            i = null;
-                            return present;
-                        }
-                    }
-                }catch(e){
-                    return null;
-                }
-            }
-        }
-        return null;
+    addtest("native-navigator", function(g) {
+        return ("navigator" in g);
     });
-    
+
     /**
      * geolocation tests for the new Geolocation API specification.
      *   This test is a standards compliant-only test; for more complete
@@ -79,22 +51,22 @@
      *   http://gist.github.com/366184
      */
 
-    addtest("native-geolocation", function() {
-        return !!navigator.geolocation;
+    addtest("native-geolocation", function(g) {
+        return has("native-navigator") && ("geolocation" in g.navigator);
     });
 
     addtest("native-crosswindowmessaging", function(g) {
-        return !!g.postMessage;
+        return ("postMessage" in g);
     });
-        
+
     addtest("native-orientation",function(g){
         return ("ondeviceorientation" in g);
     });
-    
+
     /*
      * not sure if there is any point in testing for worker support
-     * as an adequate fallback is impossible/pointless 
-     * 
+     * as an adequate fallback is impossible/pointless
+     *
      * ^rw
      */
 
@@ -104,81 +76,81 @@
 
     addtest("native-sharedworker", function(g){
         return ("SharedWorker" in g);
-    });    
-    
+    });
+
     addtest("native-eventsource", function(g){
         return ("EventSource" in g);
     });
-    
+
     // non-browser specific
     addtest("eval-global-scope", function(g){
         var fnId = "__eval" + Number(new Date()),
-            passed = false;
+            supported = false;
 
+        // catch indirect eval call errors (i.e. in such clients as Blackberry 9530)
         try{
-            // catch indirect eval call errors (i.e. in such clients as Blackberry 9530)
             g.eval("var " + fnId + "=true");
         }catch(e){}
-        passed = (g[fnId] === true);
-        if(passed){
+
+        supported = (g[fnId] === true);
+        if(supported){
             try{
                 delete g[fnId];
             }catch(e){
-                g[fnId] = void 0;
+                g[fnId] = undefined;
             }
         }
-        return passed;
+        return supported;
     });
-    
+
     // in chrome incognito mode, openDatabase is truthy, but using it
     //   will throw an exception: http://crbug.com/42380
-    // we create a dummy database. there is no way to delete it afterwards. sorry. 
+    // we create a dummy database. there is no way to delete it afterwards. sorry.
     addtest("native-sql-db", function(g){
-        var result = !!g.openDatabase,
-            dbname = "hasjstestdb";
-            
-        if(result){
+        var dbname = "hasjstestdb",
+            supported = ("openDatabase" in g);
+
+        if(supported){
             try{
-                result = !!openDatabase( dbname, "1.0", dbname, 2e4);
+                supported = !!openDatabase( dbname, "1.0", dbname, 2e4);
             }catch(e){
-                result = false;
+                supported = false;
             }
         }
-        return result;
+        return supported;
     });
-    
+
     // FIXME: hosttype
     // FIXME: moz and webkit now ship this prefixed. check all possible prefixes. ^pi
     addtest("native-indexeddb", function(g){
-        return !!g["indexedDB"];
+        return ("indexedDB" in g);
     });
-    
-    
+
+
     addtest("native-localstorage", function (g) {
       //  Thanks Modernizr!
+      var supported = false;
       try {
-        return (g.localStorage && localStorage.setItem);
-      } catch(e) {
-        return false;
-      }      
+        supported = ("localStorage" in g) && ("setItem" in localStorage);
+      } catch(e){}
+      return supported;
     });
 
     addtest("native-sessionstorage", function (g) {
       //  Thanks Modernizr!
+      var supported = false;
       try {
-        return (g.sessionStorage && sessionStorage.setItem);
-      } catch(e){
-        return false;
-      }    
+        supported = ("sessionStorage" in g) && ("setTime" in sessionStorage);
+      } catch(e){}
+      return supported;
     });
-    
-    
+
     addtest("native-history-state", function(g){
-        return (g.history && history.pushState);
+        return ("history" in g) && ("pushState" in history);
     });
-    
+
     addtest("native-websockets", function(g){
         return ("WebSocket" in g);
     });
-    
+
 })(has, has.add, has.cssprop);
