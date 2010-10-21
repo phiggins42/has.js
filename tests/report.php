4b6114177fbc4b4b563c8073b5faece5a521ed51
Peter E Higgins <dante@dojotoolkit.org>
2010-10-21 19:12:22 -0400

    should have saved changes before last commit

--- tests/report.php
+++ tests/report.php
@@ -1,8 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>has.js submissions</title>
+</head>
+<body>
+  
+    <h1>FTW</h1>
 
-$data = json_encode($_POST);
-$fp = fopen("results.data", "a+");
-fputs($fp, $data . "\n");
-fclose($fp);
+    <?php
 
-print "<h1>Thanks</h1>";
+    if(!empty($_POST)){
+        $data = json_encode($_POST);
+        $fp = @fopen("results.data", "a+");
+        if($fp){
+            fputs($fp, $data . "\n");
+            fclose($fp);
+            print "<p>Data stashed. Really, <em>Thanks!</em></p>";
+        }else{
+            print "<p>It appears there was an issue.</p>";
+        }
+    }else{
+        print "<p>No data set sent along?</p>";
+    }
+
+    
+
+?>
+<p><a href="runTests.html">back to runTests.html</a></p>
+</body>
+</html>
