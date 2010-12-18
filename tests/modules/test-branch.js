d63c6489c53f3672a88a5dd0c541930590850f8d
Kris Zyp <kriszyp@gmail.com>
2010-12-18 15:48:25 -0700

    Added test for empty string/null module

--- tests/modules/test-branch.js
+++ tests/modules/test-branch.js
@@ -1,7 +1,9 @@
-define(["has!dom?./a:./b", "./has-plugin-test!true?false?./a:false?./b:./c:./d"], function(dom, branch){
+define(["has!dom?./a:./b", "./has-plugin-test!true?false?./a:false?./b:./c:./d", "./has-plugin-test!false?a:"], function(dom, branch, none){
 	console.log("dom", dom);
 	console.log("branch", branch);
+	console.log("none", none);
 	console.assert(dom === "a");
 	console.assert(branch === "c");
+	console.assert(none === null);
 	console.log("passed tests");
 });
