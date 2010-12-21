9b926f49ab1cc59715a05809116bffb1905f46f1
Kris Zyp <kriszyp@gmail.com>
2010-12-21 15:53:40 -0700

    Add test using existing detect module

--- tests/modules/test-branch.js
+++ tests/modules/test-branch.js
@@ -1,6 +1,7 @@
-define(["has!dom?./a:./b", "./has-plugin-test!true?false?./a:false?./b:./c:./d", "./has-plugin-test!false?a:"], function(dom, branch, none){
-	console.assert(dom === "a","a");
-	console.assert(branch === "c", "c");
+define(["has!dom?./a:./b", "detect/array!array-every?./a:./b", "./has-plugin-test!true?false?./a:false?./b:./c:./d", "./has-plugin-test!false?a"], function(dom, every, branch, none){
+	console.assert(dom === "a","dom");
+	console.assert(every === ([].every ? "a": "b"), "every");
+	console.assert(branch === "c", "branch");
 	console.assert(none === undefined, "undefined");
 	console.log("passed tests");
 });
