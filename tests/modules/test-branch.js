23377f8297e9c150ca6e3f9834ba8a7db6e4d297
Kris Zyp <kriszyp@gmail.com>
2010-12-21 15:26:11 -0700

    Fixed asserts, removed unnecessary log statements

--- tests/modules/test-branch.js
+++ tests/modules/test-branch.js
@@ -1,9 +1,6 @@
 define(["has!dom?./a:./b", "./has-plugin-test!true?false?./a:false?./b:./c:./d", "./has-plugin-test!false?a:"], function(dom, branch, none){
-	console.log("dom", dom);
-	console.log("branch", branch);
-	console.log("none", none);
-	console.assert(dom === "a");
-	console.assert(branch === "c");
-	console.assert(none === null);
+	console.assert(dom === "a","a");
+	console.assert(branch === "c", "c");
+	console.assert(none === undefined, "undefined");
 	console.log("passed tests");
 });
