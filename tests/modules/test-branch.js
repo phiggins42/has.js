85e035dcb7dacf5df367948c75649474915f3268
Kris Zyp <kriszyp@gmail.com>
2010-12-23 22:49:57 -0700

    Update to use package config and new package handling

--- tests/modules/test-branch.js
+++ tests/modules/test-branch.js
@@ -1,4 +1,4 @@
-define(["has!dom?./a:./b", "detect/array!array-every?./a:./b", "./has-plugin-test!true?false?./a:false?./b:./c:./d", "./has-plugin-test!false?a"], function(dom, every, branch, none){
+define(["has!dom?./a:./b", "has/array!array-every?./a:./b", "./has-plugin-test!true?false?./a:false?./b:./c:./d", "./has-plugin-test!false?a"], function(dom, every, branch, none){
 	console.assert(dom === "a","dom");
 	console.assert(every === ([].every ? "a": "b"), "every");
 	console.assert(branch === "c", "branch");
