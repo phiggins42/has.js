91ceca0edefeab2642cd46be030bd03c7262e3ce
Kris Zyp <kriszyp@gmail.com>
2010-12-27 17:35:08 -0700

    Fix has checking

--- _plugin.js
+++ _plugin.js
@@ -1,13 +1,17 @@
-define(["require", "./has"], function(require){
+define(["require"], function(require){
 	// this allows us to use has as a dependency plugin for RequireJS and other AMD loaders (like Dojo). The syntax supports the ternary operation for branching:
 	// define("my-module", ["has!array-every?module-using-every:module-using-for-loop"], function(arrayModule){
 	// });
 	var has;
-	return function(id, parentRequire, loaded){
+	return function(id, parentRequire, loaded, config){
 		// split into the different branches, based on has features
 		// first parse the id
 		var tokens = id.match(/[\?:]|[^:\?]*/g), i = 0;
-		has = has || require("./has");
+		has = has || config.isBuild ?
+			function(feature){
+				return config[feature];
+			} :
+			require("has");
 		function get(skip){
 			var operator, term = tokens[i++];
 			if(term == ":"){
@@ -16,7 +20,7 @@ define(["require", "./has"], function(require){
 			}else{
 				// postfixed with a ? means it is a feature to branch on, the term is the name of the feature
 				if(tokens[i++] == "?"){
-					if(!skip && checkResult(has(term))){
+					if(!skip && has(term)){
 						// matched the feature, get the first value from the options 
 						return get();
 					}else{
@@ -36,10 +40,4 @@ define(["require", "./has"], function(require){
 			loaded();
 		}
 	};
-	function checkResult(result){
-		if(typeof result == "undefined"){
-			throw new Error("No test " + term + " was defined");
-		}
-		return result;
-	}
 });
